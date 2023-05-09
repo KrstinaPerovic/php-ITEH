2ee428ea79bc4cdfb091bde80592e178a3caaebf
KrstinaPerovic <perovickrstina1@gmail.com>
2023-05-09 15:25:59 +0200

    prikazana sva putovanja na pocetnoj stranici

--- index.php
+++ index.php
@@ -1,12 +1,57 @@
+<?php
+
+require_once 'model/Putovanje.php';
+
+$putovanja = Putovanje::getPutovanja();
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Putovanja</title>
 </head>
 <body>
-    <h1>Zdravo</h1>
+        <h1>Sva putovanja</h1>
+        <?php if (count($putovanja) > 0): ?>
+            <table>
+            <thead>
+                <tr>
+                <th>Putovanje ID</th>
+                <th>Destinacija</th>
+                <th>Datum</th>
+                <th>Trajanje putovanja</th>
+                <th>Cena</th>
+                <th>Korisnik ID</th>
+                <th>Ime i prezime korisnika</th>
+                <th>Email korisnika</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($putovanja as $putovanje): ?>
+                <tr>
+                    <td><?php echo $putovanje["putovanje_id"]; ?></td>
+                    <td><?php echo $putovanje["destinacija"]; ?></td>
+                    <td><?php echo $putovanje["datum"]; ?></td>
+                    <td><?php echo $putovanje["trajanje_putovanja"]; ?></td>
+                    <td><?php echo $putovanje["cena"]; ?></td>
+                    <td><?php echo $putovanje["korisnik_id"]; ?></td>
+                    <td><?php echo $putovanje["ime"] . " " . $putovanje["prezime"]; ?></td>
+                    <td><?php echo $putovanje["email"]; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+            </table>
+        <?php else: ?>
+            <p>Nema putovanja.</p>
+        <?php endif; ?>
+</body>
+</html>
+
 </body>
 </html>
--- model/Putovanje.php
+++ model/Putovanje.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/../DatabaseBroker.php';
 
  
 class Putovanje {
@@ -45,6 +45,51 @@ class Putovanje {
     public function getKorisnikId() {
       return $this->korisnik_id;
     }
+
+
+
+
+    public static function getPutovanja() {
+        $conn = DatabaseBroker::getConnection();
+        $sql = "SELECT p.*, k.ime, k.prezime, k.email FROM putovanje p JOIN korisnik k ON p.korisnik_id = k.korisnik_id";
+        $result = $conn->query($sql);
+    
+        $putovanja = [];
+    
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            $putovanje = [
+              "putovanje_id" => $row["putovanje_id"],
+              "destinacija" => $row["destinacija"],
+              "datum" => $row["datum"],
+              "trajanje_putovanja" => $row["trajanje_putovanja"],
+              "cena" => $row["cena"],
+              "korisnik_id" => $row["korisnik_id"],
+              "ime" => $row["ime"],
+              "prezime" => $row["prezime"],
+              "email" => $row["email"]
+            ];
+    
+            $putovanja[] = $putovanje;
+          }
+        }
+    
+        $conn->close();
+    
+        return $putovanja;
+      }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
